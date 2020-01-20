a66c7823d954c80560d431cf066c942fe440a657
Rafael Brandao <r.augusto.brandao@gmail.com>
2020-01-20 17:13:43 -0500

    Added Web module tp project

---
+++ web/src/main/java/brandao/rafael/NutriU/NutriUApplication.java
@@ -0,0 +1,12 @@
+package brandao.rafael;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NutriUApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NutriUApplication.class, args);
+    }
+}
